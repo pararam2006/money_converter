a18f0ab4274926cc5abe22a867731d34d4cc6eb5
Андрей <benderkott@gmail.com>
2025-01-21 12:25:11 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
@@ -14,28 +13,73 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key})
+  const MyApp({super.key});
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  String dropdownValue = 'Рубли';
+  static const List<String> dropdownItems = [
+    'Рубли',
+    'Доллары',
+    'Письки бобра'
+  ];
   double firstValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        width: 400,
-        height: 400,
-        child: DropdownButton(
-            items: [
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Expanded(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  TextField(),
+                  DropdownButton(
+                    value: dropdownValue,
+                    items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (selectedPosition) {
+                      setState(() {
+                        dropdownValue = selectedPosition!;
+                      });
+                    }
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  TextField(),
+                  DropdownButton(
+                    value: dropdownValue,
+                    items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (selectedPosition) {
+                      setState(() {
+                        dropdownValue = selectedPosition!;
+                      });
+                    }
+                  ),
+                ],
+              )
+            ]
 
-            ],
-            onChanged: (selectedPosition) {
-
-            }),
+          ),
+        ),
       )
     );
   }
